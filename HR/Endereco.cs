137bc8b09c679156d980297ee02efeb2881c0378
Vinicius Petratti <viniciuspetratti@hotmail.com>
2022-03-22 19:52:28 -0300

    Classes criadas

---
+++ HR/Endereco.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR
+{
+    internal class Endereco
+    {
+        public string rua { get;  set; }
+        public string numero { get;  set; }
+        public string complemento { get;  set; }
+        public string cep { get;  set; }
+
+        public Endereco(string rua, string numero, string complemento, string cep)
+        {
+            this.rua = rua;
+            this.numero = numero;
+            this.complemento = complemento;
+            this.cep = cep;
+        }
+    }
+}
---
+++ HR/Funcionário.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR
+{
+    internal class Funcionário
+    {
+        public int id { get;  set; }
+        public string nome { get;  set; }
+        public string cpf { get;  set; }
+        public Endereco endereco { get;  set; }
+
+        public Funcionário(int id, string nome, string cpf, Endereco endereco)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.cpf = cpf;
+            this.endereco = endereco;
+        }
+    }
+}
